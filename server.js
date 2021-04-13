548a60ff7a520f30097b9d564b8ee1331e069ed8
kollosp <pparczyk@wp.pl>
2021-04-13 15:50:58 +0200

    added http

--- server.js
+++ server.js
@@ -3,8 +3,31 @@ require('./logger')
 const CONST = require('./constModule')()
 
 const WebSocket = require('ws');
+const http = require('http');
+let wss;
+let httpServer;
 
-const wss = new WebSocket.Server({ port: CONST.wsport });
+
+if(CONST.httpServer) {
+    console.log("Run websocket server stand alone")
+    wss = new WebSocket.Server({port: CONST.wsport});
+}else{
+    console.log("Run websocket server on http server")
+    httpServer = http.createServer();
+    wss = new WebSocket.Server({noServer: true});
+
+    httpServer.on('')
+
+    httpServer.on('upgrade', function upgrade(request, socket, head) {
+        console.log("Received upgrade.")
+
+        wss.handleUpgrade(request, socket, head, function done(ws) {
+            wss.emit('connection', ws, request);
+        });
+    })
+
+    httpServer.listen(CONST.wsport)
+}
 
 wss.on('connection', function connection(ws, req) {
 
